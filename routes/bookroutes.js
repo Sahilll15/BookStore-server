810ea169fbc8a691b81faf7a1bd5e65bdb9eda1e
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2023-06-08 01:48:41 +0530

    new router

--- routes/bookroutes.js
+++ routes/bookroutes.js
@@ -7,6 +7,7 @@ const { verifySuperUserToken, verifytoken } = require('../middleware/verifytoken
 
 router.post('/addbook', verifySuperUserToken, addBook)
 router.get('/getbooks', verifytoken, getBooks)
+router.get('/superuser/getbooks', verifySuperUserToken, getBooks)
 router.get('/getbook/:id', verifytoken, getBookbyID)
 router.delete('/delete/:id', verifySuperUserToken, deletebook)
 router.patch('/update/:id', verifySuperUserToken, updatebook)
